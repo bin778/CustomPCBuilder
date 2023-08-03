8c6ab74a1c31169cd802b7ddd4f7d1e98a648d17
최영빈 <bin778@naver.com>
2023-08-03 21:01:17 +0900

    feat Main

--- frontend/src/view/Main.jsx
+++ frontend/src/view/Main.jsx
@@ -1,21 +1,42 @@
 import { useNavigate } from "react-router-dom";
 
+// 이미지 파일 목록
+import LOGIN_LOGO from "../images/login-logo.png";
+
+// SCSS 파일
+import "../css/Main.scss"
+
 export default function Main() {
   const navigate = useNavigate();
 
+  // 회원 가입 페이지 이동
   const goToSignUp = () => {
     navigate("/sign_up");
   }
 
+  // 로그인 페이지 이동
   const goToLogin = () => {
     navigate("/login");
   }
 
   return (
-    <div>
-      <span onClick={goToSignUp}>회원가입</span>
-      <span> </span>
-      <span onClick={goToLogin}>로그인</span>
+    <div className="main-layer">
+      <div className="header">
+        {/* 메인 로고 */}
+        <span className="main-logo">CUSTOMPCBUILDER</span>
+        {/* 메인 메뉴 */}
+        <span className="main-menu">온라인 견적</span>
+        <span className="main-menu">맞춤 견적</span>
+        <span className="main-menu">PC부품 검색</span>
+        <span className="main-menu">질문 게시판</span>
+        {/* 로그인 메뉴 */}
+        <span className="login-menu" onClick={goToLogin}>로그인</span>
+        <span className="login-menu" onClick={goToSignUp}>회원가입</span>
+        <img src={LOGIN_LOGO} className="login-logo" alt="" />
+      </div>
+      <div className="body">
+
+      </div>
     </div>
   )
 }
