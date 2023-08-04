9c9c83d36426b34d73e1c64c3973481330d6a2f0
최영빈 <bin778@naver.com>
2023-08-04 19:21:07 +0900

    feat image

--- frontend/src/view/Main.jsx
+++ frontend/src/view/Main.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 // 이미지 파일 목록
 import LOGIN_LOGO from "../images/login-logo.png";
+import COMPUTER from "../images/computer.png";
 
 // SCSS 파일
 import "../css/Main.scss"
@@ -35,7 +36,9 @@ export default function Main() {
         <img src={LOGIN_LOGO} className="login-logo" alt="" />
       </div>
       <div className="body">
-
+        <img src={COMPUTER} className="computer" alt="" />
+        <div className="main-title">아직도 컴퓨터 견적을 어떻게 맞춰야 되는지 모르겠다고요?</div>
+        <div className="main-context">'맞춤견적'에서 키워드를 입력하면 원하는 사양과 가격의 컴퓨터를 맞출 수 있습니다.</div>
       </div>
     </div>
   )
