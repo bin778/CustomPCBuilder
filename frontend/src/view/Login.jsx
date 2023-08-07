488946cb564f2e9655f5763f683e712702fe8529
최영빈 <bin778@naver.com>
2023-08-07 20:03:05 +0900

    feat login

--- frontend/src/view/Login.jsx
+++ frontend/src/view/Login.jsx
@@ -1,5 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
+// SCSS 파일
+import "../css/Login.scss"
+
 export default function Login() {
+  const navigate = useNavigate();
+
+  // 회원 가입 페이지 이동
+  const goToSignUp = () => {
+    navigate("/sign_up");
+  }
+
   return (
-    <div>로그인 화면입니다.</div>
+    <div className="login-layer">
+      <div className="login-form">
+        <div className="login-logo">CUSTOMPCBUILDER</div>
+        <div className="container">
+          <input type="text" placeholder="아이디" id="id" className="login-input" />
+          <input type="password" placeholder="비밀번호" id="password" className="login-input" />
+          <button id="login" className="login-button">로그인</button>
+          <span>계정이 없으세요?</span>
+          <span onClick={goToSignUp}>회원가입</span>
+        </div>
+      </div>
+    </div>
   )
 }
--- frontend/src/view/Main.jsx
+++ frontend/src/view/Main.jsx
@@ -22,7 +22,7 @@ export default function Main() {
 
   return (
     <div className="main-layer">
-      <div className="header">
+      <div className="main-header">
         {/* 메인 로고 */}
         <span className="main-logo">CUSTOMPCBUILDER</span>
         {/* 메인 메뉴 */}
@@ -35,7 +35,7 @@ export default function Main() {
         <span className="login-menu" onClick={goToSignUp}>회원가입</span>
         <img src={LOGIN_LOGO} className="login-logo" alt="" />
       </div>
-      <div className="body">
+      <div className="main-body">
         <img src={COMPUTER} className="computer" alt="" />
         <div className="main-title">아직도 컴퓨터 견적을 어떻게 맞춰야 되는지 모르겠다고요?</div>
         <div className="main-context">'맞춤견적'에서 검색창에서 원하는 키워드를 입력하면, 원하는 사양과 가격의 컴퓨터를 알아서 맞춰줍니다.</div>
