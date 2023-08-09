f48fba86c8d72b1be8948e6f9d2913e44027d7da
최영빈 <bin778@naver.com>
2023-08-09 20:26:16 +0900

    feat login

--- frontend/src/view/Login.jsx
+++ frontend/src/view/Login.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 // SCSS 파일
 import "../css/Login.scss"
 
 export default function Login() {
+  let [id, setId] = useState('');
+  let [pw, setPw] = useState('');
+
   const navigate = useNavigate();
+
+  const realName = "최영빈"; 
+  const realId = "bin778@naver.com";
+  const realPw = "12345678";
 
   // 회원 가입 페이지 이동
   const goToSignUp = () => {
     navigate("/sign_up");
+  }
+
+  // 메인 페이지로 이동
+  const goToMain = () => {
+    navigate("/");
   }
 
   return (
@@ -16,9 +29,25 @@ export default function Login() {
       <div className="login-form">
         <div className="login-logo">CUSTOMPCBUILDER</div>
         <div>
-          <div><input type="text" placeholder="아이디" id="id" className="login-input" /></div>
-          <div><input type="password" placeholder="비밀번호" id="password" className="login-input" /></div>
-          <div><button id="login" className="login-button">로그인</button></div>
+          <div>
+            <input type="text" placeholder="E-Mail" id="id" className="login-input" onChange={e => {
+              setId(e.target.value);
+            }}/>
+          </div>
+          <div>
+            <input type="password" placeholder="Password" id="password" className="login-input" onChange={e => {
+              setPw(e.target.value);
+            }}/>
+          </div>
+          <div><button type="button" className="login-button" onClick={e => {
+            if (realId === id && realPw === pw) {
+              alert(realName + '님 환영합니다.');
+              e.stopPropagation();
+              goToMain();
+            } else {
+              alert('이메일 혹은 패스워드가 일치하지 않습니다.');
+            }
+          }}>로그인</button></div>
           <div>
             <span>계정이 없으세요? </span>
             <span className="signup-button" onClick={goToSignUp}>회원가입</span>
