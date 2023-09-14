e77c16e1fee900966135f2d67c2cfaeb46e48ede
최영빈 <bin778@naver.com>
2023-09-14 21:51:00 +0900

    feat quote

--- frontend/src/view/Quote.jsx
+++ frontend/src/view/Quote.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 // 이미지 파일 목록
 import COPY from "../images/copy.png";
 import COMPATIBLE from "../images/compatible.png";
@@ -7,9 +9,9 @@ import PRICE from "../images/price.png";
 // CPU 이미지
 import RYZEN9 from "../images/CPU/RYZEN9.jpg"
 import RYZEN5 from "../images/CPU/RYZEN5.jpg"
-import I7 from "../images/CPU/i7.jpg"
+// import I7 from "../images/CPU/i7.jpg"
 import I5 from "../images/CPU/i5.jpg"
-import I3 from "../images/CPU/i3.jpg"
+// import I3 from "../images/CPU/i3.jpg"
 
 // SCSS 파일
 import "../css/Quote.scss"
@@ -18,6 +20,9 @@ import "../css/Quote.scss"
 import Header from "./Component/Header.jsx";
 
 export default function Quote() {
+  // 온라인 견적 카테고리 버튼
+  let [btnActive, setBtnActive] = useState('cpu');
+
   return (
     <div className="quote-layer">
       <Header />
@@ -44,52 +49,74 @@ export default function Quote() {
         </div>
         <div>
           <span className="option-list">
-            <div className="list-line"></div>
-            <div className="option-title">제조사</div>
-            <div><input type="checkbox"></input><span className="option-content">인텔</span></div>
-            <div><input type="checkbox"></input><span className="option-content">AMD</span></div>
-            <div className="list-line"></div>
-            <div className="option-title">소켓</div>
-            <div><input type="checkbox"></input><span className="option-content">AM4</span></div>
-            <div><input type="checkbox"></input><span className="option-content">AM5</span></div>
-            <div><input type="checkbox"></input><span className="option-content">LGA1700</span></div>
-            <div className="list-line"></div>
+            {/* CPU 옵션 필터 */}
+            <span className={(btnActive === 'cpu' ? '' : 'hidden')}>
+              <div className="list-line"></div>
+              <div className="option-title">제조사</div>
+              <div><input type="checkbox"></input><span className="option-content">AMD</span></div>
+              <div><input type="checkbox"></input><span className="option-content">인텔</span></div>
+              <div className="list-line"></div>
+              <div className="option-title">코어 수</div>
+              <div><input type="checkbox"></input><span className="option-content">4</span></div>
+              <div><input type="checkbox"></input><span className="option-content">6</span></div>
+              <div><input type="checkbox"></input><span className="option-content">12</span></div>
+              <div className="list-line"></div>
+              <div className="option-title">소켓</div>
+              <div><input type="checkbox"></input><span className="option-content">AM4</span></div>
+              <div><input type="checkbox"></input><span className="option-content">AM5</span></div>
+              <div><input type="checkbox"></input><span className="option-content">LGA1700</span></div>
+              <div className="list-line"></div>
+            </span>
+            {/* 쿨러 옵션 필터 */}
+            <span className={(btnActive === 'cooler' ? '' : 'hidden')}>
+              <div className="list-line"></div>
+              <div className="option-title">제조사</div>
+              <div><input type="checkbox"></input><span className="option-content">3RSYS</span></div>
+              <div><input type="checkbox"></input><span className="option-content">NZXT</span></div>
+              <div><input type="checkbox"></input><span className="option-content">쿨러마스터</span></div>
+              <div className="list-line"></div>
+              <div className="option-title">냉각 방식</div>
+              <div><input type="checkbox"></input><span className="option-content">공랭</span></div>
+              <div><input type="checkbox"></input><span className="option-content">수랭</span></div>
+              <div className="list-line"></div>
+            </span>
           </span>
           <span className="product-list">
-              <div className="product">
-                <div className="list-line"></div>
-                <img src={RYZEN9} className="product-image" alt="" />
-                <span className="product-name">AMD 라이젠9-5900X</span>
-                <span className="product-spec">12코어 / 24쓰레드 / 3.7Ghz / AM4 / 105W</span>
-                <span className="product-price">499,900원</span>
-                <div className="list-line"></div>
-              </div>
-              <div className="product">
-                <div className="list-line"></div>
-                <img src={RYZEN5} className="product-image" alt="" />
-                <span className="product-name">AMD 라이젠5-7600X</span>
-                <span className="product-spec">6코어 / 12쓰레드 / 3.8Ghz / AM5 / 105W</span>
-                <span className="product-price">378,000원</span>
-                <div className="list-line"></div>
-              </div>
-              <div className="product">
-                <div className="list-line"></div>
-                <img src={I5} className="product-image" alt="" />
-                <span className="product-name">인텔 i5-12400</span>
-                <span className="product-spec">6코어 / 12쓰레드 / 2.5Ghz / LGA1700 / 65W</span>
-                <span className="product-price">229,500원</span>
-                <div className="list-line"></div>
-              </div>
+            {/* CPU 상품 */}
+            <div className={(btnActive === 'cpu' ? 'product' : 'hidden')}>
+              <div className="list-line"></div>
+              <img src={RYZEN9} className="product-image" alt="" />
+              <span className="product-name">AMD 라이젠9-5900X</span>
+              <span className="product-spec">12코어 / 24쓰레드 / 3.7Ghz / AM4 / 105W</span>
+              <span className="product-price">499,900원</span>
+              <div className="list-line"></div>
+            </div>
+            <div className={(btnActive === 'cpu' ? 'product' : 'hidden')}>
+              <div className="list-line"></div>
+              <img src={RYZEN5} className="product-image" alt="" />
+              <span className="product-name">AMD 라이젠5-7600X</span>
+              <span className="product-spec">6코어 / 12쓰레드 / 3.8Ghz / AM5 / 105W</span>
+              <span className="product-price">378,000원</span>
+              <div className="list-line"></div>
+            </div>
+            <div className={(btnActive === 'cpu' ? 'product' : 'hidden')}>
+              <div className="list-line"></div>
+              <img src={I5} className="product-image" alt="" />
+              <span className="product-name">인텔 i5-12400</span>
+              <span className="product-spec">6코어 / 12쓰레드 / 2.5Ghz / LGA1700 / 65W</span>
+              <span className="product-price">229,500원</span>
+              <div className="list-line"></div>
+            </div>
           </span>
           <span className="cart-list">
-            <div className="cart cart-selected">CPU</div>
-            <div className="cart">쿨러</div>
-            <div className="cart">메인보드</div>
-            <div className="cart">메모리</div>
-            <div className="cart">그래픽카드</div>
-            <div className="cart">저장공간</div>
-            <div className="cart">파워</div>
-            <div className="cart">케이스</div>
+            <div className={"cart" + (btnActive === 'cpu' ? ' active' : '')} onClick={() => { setBtnActive('cpu'); }}>CPU</div>
+            <div className={"cart" + (btnActive === 'cooler' ? ' active' : '')} onClick={() => { setBtnActive('cooler'); }}>쿨러</div>
+            <div className={"cart" + (btnActive === 'mainboard' ? ' active' : '')} onClick={() => { setBtnActive('mainboard'); }}>메인보드</div>
+            <div className={"cart" + (btnActive === 'memory' ? ' active' : '')} onClick={() => { setBtnActive('memory'); }}>메모리</div>
+            <div className={"cart" + (btnActive === 'videocard' ? ' active' : '')} onClick={() => { setBtnActive('videocard'); }}>비디오카드</div>
+            <div className={"cart" + (btnActive === 'storage' ? ' active' : '')} onClick={() => { setBtnActive('storage'); }}>저장공간</div>
+            <div className={"cart" + (btnActive === 'power' ? ' active' : '')} onClick={() => { setBtnActive('power'); }}>파워</div>
+            <div className={"cart" + (btnActive === 'comcase' ? ' active' : '')} onClick={() => { setBtnActive('comcase'); }}>케이스</div>
           </span>
         </div>
       </div>
