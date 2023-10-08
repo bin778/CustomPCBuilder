8efc2b528217f07b1aaec0fc82a93cfc61b531af
최영빈 <bin778@naver.com>
2023-10-08 23:45:02 +0900

    fix keyword

--- backend/src/cpb_django/ml_quote/keyword.py
+++ backend/src/cpb_django/ml_quote/keyword.py
@@ -13,10 +13,10 @@ gb = hannanum.morphs("게임방송용 BJ용")
 
 # 가정용 견적 키워드
 web = hannanum.morphs("웹서핑용 인터넷검색용 가정용")
-video = hannanum.morphs("영상시청용 인강용 인터넷강의용 동영상강의용")
+vwatch = hannanum.morphs("영상시청용 인강용 인터넷강의용 동영상강의용")
 slim = hannanum.morphs("슬림용 미니용")
 
 # 작업용 견적 키워드
 office = hannanum.morphs("사무용 문서작업용")
-coding = hannanum.morphs("개발용 코딩용")
-edit = hannanum.morphs("동영상편집용 영상편집용")
+coding = hannanum.morphs("개발용 개발자용 코딩용")
+vedit = hannanum.morphs("동영상편집용 영상편집용")
