0622daf3d02caadf4ece8a38a03fafcd732fa9b3
최영빈 <bin778@naver.com>
2023-10-09 23:21:48 +0900

    fix keyword

--- backend/src/cpb_django/ml_quote/keyword.py
+++ backend/src/cpb_django/ml_quote/keyword.py
@@ -5,18 +5,38 @@ from konlpy.tag import Hannanum
 hannanum = Hannanum()
 
 # 게임용 견적 키워드
-lol = hannanum.morphs("LOL용 롤용 리그오브레전드용")
-fifa4 = hannanum.morphs("피파4용 피파용 피파온라인4용 피파온라인용")
-pubg = hannanum.morphs("배그용 배틀그라운드용 PUBG용")
-cp = hannanum.morphs("사이버펑크용 사펑용")
-gb = hannanum.morphs("게임방송용 BJ용")
+lol = hannanum.nouns("롤용 리그오브레전드용")
+fifa4 = hannanum.nouns("피파4용 피파용 피온용")
+pubg = hannanum.nouns("배그용 배틀그라운드용")
+cp = hannanum.nouns("사이버펑크용 사펑용")
+gb = hannanum.nouns("게임방송용 스트리머용")
 
 # 가정용 견적 키워드
-web = hannanum.morphs("웹서핑용 인터넷검색용 가정용")
-vwatch = hannanum.morphs("영상시청용 인강용 인터넷강의용 동영상강의용")
-slim = hannanum.morphs("슬림용 미니용")
+web = hannanum.nouns("웹서핑용 인터넷검색용 가정용")
+vwatch = hannanum.nouns("영상시청용 인강용 인터넷강의용")
+slim = hannanum.nouns("슬림용 미니용")
 
 # 작업용 견적 키워드
-office = hannanum.morphs("사무용 문서작업용")
-coding = hannanum.morphs("개발용 개발자용 코딩용")
-vedit = hannanum.morphs("동영상편집용 영상편집용")
+office = hannanum.nouns("사무용 문서작업용")
+coding = hannanum.nouns("개발용 개발자용 코딩용")
+vedit = hannanum.nouns("동영상편집용 영상편집용")
+
+def main(request):
+  # 게임용 키워드 출력
+  print(lol)
+  print(fifa4)
+  print(pubg)
+  print(cp)
+  print(gb)
+
+  # 가정용 키워드 출력
+  print(web)
+  print(vwatch)
+  print(slim)
+
+  # 작업용 키워드 출력
+  print(office)
+  print(coding)
+  print(vedit)
+
+  return HttpResponse("KoNNPy를 사용한 키워드 추출")
--- backend/src/cpb_django/ml_quote/urls.py
+++ backend/src/cpb_django/ml_quote/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from . import views
+from . import keyword
 
 urlpatterns = [
-    path('', views.main),
+    path('', keyword.main),
 ]
