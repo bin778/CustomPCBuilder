1c19636790ceaad7f17cc96011e7ac8a89dd6f04
최영빈 <bin778@naver.com>
2023-10-11 23:27:18 +0900

    feat keyword

--- frontend/src/view/Keyword.jsx
+++ frontend/src/view/Keyword.jsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from 'axios'
 
+// 이미지 파일 목록
+import SEARCH from "../images/search.png";
+
+// SCSS 파일
+import "../css/Keyword.scss"
+
 // 컴포넌트 파일
 import Header from "./Component/Header.jsx";
 
@@ -8,6 +14,14 @@ export default function Keyword() {
   return (
     <div className="keyword-layer">
       <Header />
+      <div className="keyword-body">
+        <div>
+        <div className="search-title">원하는 용도를 입력하세요!</div>
+          <img src={SEARCH} className="search-image" alt="" />
+          <input type="text" id="keyword" className="search-input" />
+        </div>
+        <span className="search-button">검색</span>
+      </div>
     </div>
   )
 }
