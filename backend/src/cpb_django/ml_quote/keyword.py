46aa73038ae9951f4a9f0a3284bc84d531bde634
최영빈 <bin778@naver.com>
2023-10-14 23:06:46 +0900

    fix keyword

--- backend/src/cpb_django/ml_quote/keyword.py
+++ backend/src/cpb_django/ml_quote/keyword.py
@@ -8,39 +8,44 @@ hannanum = Hannanum()
 lol = hannanum.nouns("롤용 리그오브레전드용")
 fifa4 = hannanum.nouns("피파4용 피파용 피온용")
 pubg = hannanum.nouns("배그용 배틀그라운드용")
-cp = hannanum.nouns("사이버펑크용 사펑용")
-gb = hannanum.nouns("게임방송용 스트리머용")
+gbroad = hannanum.nouns("게임방송용 스트리머용")
 
 # 가정용 견적 키워드
 web = hannanum.nouns("웹서핑용 인터넷검색용 가정용")
 vwatch = hannanum.nouns("영상시청용 인강용 인터넷강의용")
-slim = hannanum.nouns("슬림용 미니용")
+office = hannanum.nouns("사무용 문서작업용")
 
 # 작업용 견적 키워드
-office = hannanum.nouns("사무용 문서작업용")
 coding = hannanum.nouns("개발용 개발자용 코딩용")
 vedit = hannanum.nouns("동영상편집용 영상편집용")
+
+# 기타 견적 키워드
+slim = hannanum.nouns("슬림형 미니형")
+noise = hannanum.nouns("저소음 무소음")
 
 def main(request):
   # 게임용 견적 출력
   # print(lol)
   # print(fifa4)
   # print(pubg)
-  # print(cp)
-  # print(gb)
+  # print(gbroad)
 
   # 가정용 견적 출력
   # print(web)
   # print(vwatch)
-  # print(slim)
-
-  # 작업용 견적 출력
   # print(office)
+  
+  # 작업용 견적 출력
   # print(coding)
   # print(vedit)
 
+  # 기타 견적 출력
+  # print(slim)
+  # print(noise)
+
   return HttpResponse(
-    f"게임용 {lol} {fifa4} {pubg} {cp} {gb}" + 
-    f"가정용 {web} {vwatch} {slim}" +
-    f"작업용 {office} {coding} {vedit}"
+    f"게임용 {lol} {fifa4} {pubg} {gbroad}" + 
+    f"가정용 {web} {vwatch} {office}" +
+    f"작업용 {coding} {vedit}" + 
+    f"기타 {slim} {noise}"
   )
