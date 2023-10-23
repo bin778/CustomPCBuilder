d1487c691abc7b90d4ac5b097ecf684466a164c0
최영빈 <bin778@naver.com>
2023-10-23 23:53:34 +0900

    fix keyword

--- backend/src/cpb_django/ml_quote/keyword.py
+++ backend/src/cpb_django/ml_quote/keyword.py
@@ -27,50 +27,48 @@ slim = hannanum.nouns("슬림형 미니형")
 noise = hannanum.nouns("저소음 무소음")
 
 # 키워드 매핑 함수
-# def mappingKeyword():
+def mappingKeyword(keyword):
+  if (keyword == "롤용" or keyword == "웹서핑용" or  keyword == "영상시청용" or  keyword == "사무용"):
+    cpu = Cpu.objects.filter(cpu_title="i3-12100")
+    for cpu in cpu:
+      print(cpu.cpu_title)
+  
+  return cpu
+    
 
 # 키워드 검색 함수
 def searchKeyword(keyword):
-    if (keyword == lol[0] or keyword == lol[1]):
-      print(lol)
-    elif (keyword == fifa4[0] or keyword == fifa4[1] or keyword == fifa4[2]):
-      print(fifa4)
-    elif (keyword == pubg[0] or keyword == pubg[1]):
-      print(pubg)
-    elif (keyword == gbroad[0] or keyword == gbroad[1]):
-      print(gbroad)
+  if (keyword == lol[0] or keyword == lol[1]):
+    mappingKeyword("롤용")
+  elif (keyword == fifa4[0] or keyword == fifa4[1] or keyword == fifa4[2]):
+    print(fifa4)
+  elif (keyword == pubg[0] or keyword == pubg[1]):
+    print(pubg)
+  elif (keyword == gbroad[0] or keyword == gbroad[1]):
+    print(gbroad)
 
-    if (keyword == web[0] or keyword == web[1] or keyword == web[2]):
-      print(web)
-    elif (keyword == vwatch[0] or keyword == vwatch[1] or keyword == vwatch[2]):
-      print(vwatch)
-    elif (keyword == office[0] or keyword == office[1]):
-      print(office)
+  if (keyword == web[0] or keyword == web[1] or keyword == web[2]):
+    print(web)
+  elif (keyword == vwatch[0] or keyword == vwatch[1] or keyword == vwatch[2]):
+    print(vwatch)
+  elif (keyword == office[0] or keyword == office[1]):
+    print(office)
 
-    if (keyword == coding[0] or keyword == coding[1] or keyword == coding[2]):
-      print(coding)
-    elif (keyword == vedit[0] or keyword == vedit[1]):
-      print(vedit)
+  if (keyword == coding[0] or keyword == coding[1] or keyword == coding[2]):
+    print(coding)
+  elif (keyword == vedit[0] or keyword == vedit[1]):
+    print(vedit)
 
-    if (keyword == slim[0] or keyword == slim[1]):
-      print(slim)
-    elif (keyword == noise[0] or keyword == noise[1]):
-      print(noise)
+  if (keyword == slim[0] or keyword == slim[1]):
+    print(slim)
+  elif (keyword == noise[0] or keyword == noise[1]):
+    print(noise)
 
 # 메인 함수
 def main(request):
   if request.method == 'GET':
       keyword = request.GET.get('keyword')
       print(keyword)
-
-  # MySQL 데이터를 불러오기(필터 사용)
-  intel_cpu = Cpu.objects.filter(cpu_manufacturer="인텔")
-  for intel in intel_cpu:
-    print(intel.cpu_title)
-  
-  core6_cpu = Cpu.objects.filter(cpu_core=6)
-  for core6 in core6_cpu:
-    print(core6.cpu_title)
 
   searchKeyword(keyword)
 
