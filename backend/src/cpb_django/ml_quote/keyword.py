3b383eac13034aab533c64c103a4809746bcc2f8
최영빈 <bin778@naver.com>
2023-11-12 05:02:19 +0900

    fix keyword

--- backend/src/cpb_django/ml_quote/keyword.py
+++ backend/src/cpb_django/ml_quote/keyword.py
@@ -12,183 +12,222 @@ from .models import Power
 from .models import Comcase
 
 # 키워드 매핑 함수
-def mappingKeyword(keyword):
+def mappingKeyword(usage):
+  # 부품 성능 비교를 위한 임시 저장
+  cpus = []
+  memories = []
+  videocards = []
 
-  # 사양 설정하기
-  if (keyword == "피파4용" or keyword == "롤용" or keyword == "웹서핑용" or  keyword == "영상시청용" or  keyword == "사무용"):
-    cpus = Cpu.objects.filter(cpu_manufacturer="인텔", cpu_core=4)
-    if (keyword == "피파4용"):
-      videocards = Videocard.objects.filter(videocard_chipset="GTX1660 SUPER")
-    elif (keyword == "롤용" or keyword == "영상시청용"):
-      videocards = Videocard.objects.filter(videocard_chipset="GTX1650")
-    mainboards = Mainboard.objects.filter(mainboard_chipset="H610")
-    memories = Memory.objects.filter(memory_manufacturer="삼성전자", memory_capacity=16)
-  elif (keyword == "배그용"):
-    cpus = Cpu.objects.filter(cpu_manufacturer="인텔", cpu_core=6)
-    videocards = Videocard.objects.filter(videocard_chipset="RTX4060Ti")
-    mainboards = Mainboard.objects.filter(mainboard_chipset="H610")
-    memories = Memory.objects.filter(memory_manufacturer="삼성전자", memory_capacity=32)
-  elif (keyword == "게임방송용"):
-    cpus = Cpu.objects.filter(cpu_manufacturer="AMD", cpu_core=6)
-    videocards = Videocard.objects.filter(videocard_chipset="RTX4070Ti")
-    mainboards = Mainboard.objects.filter(mainboard_manufacturer="MSI", mainboard_chipset="A620")
-    memories = Memory.objects.filter(memory_manufacturer="삼성전자", memory_capacity=32)
-  elif (keyword == "코딩용" or keyword == "영상편집용"):
-    cpus = Cpu.objects.filter(cpu_manufacturer="인텔", cpu_core=12)
-    if (keyword == "코딩용"):
-      videocards = Videocard.objects.filter(videocard_chipset="GTX1660 SUPER")
-    elif (keyword == "영상편집용"):
-      videocards = Videocard.objects.filter(videocard_chipset="RTX4060Ti")
-    mainboards = Mainboard.objects.filter(mainboard_chipset="B760")
-    memories = Memory.objects.filter(memory_manufacturer="삼성전자", memory_capacity=32)
-  # 키워드 입력 예외 처리(입력한 검색어에서 지정한 키워드가 없을 경우)
-  elif (keyword == "용도"):
+  for keyword in usage:
+    # CPU, 그래픽카드 사양 설정하기
+    if (keyword == "피파4용" or keyword == "롤용" or keyword == "웹서핑용" or keyword == "영상시청용" or  keyword == "사무용"):
+      cpus.extend(Cpu.objects.filter(cpu_manufacturer="인텔", cpu_core=4))
+      if (keyword == "피파4용"):
+        videocards.extend(Videocard.objects.filter(videocard_chipset="GTX1660 SUPER"))
+      elif (keyword == "롤용" or keyword == "영상시청용"):
+        videocards.extend(Videocard.objects.filter(videocard_chipset="GTX1650"))
+      memories.extend(Memory.objects.filter(memory_manufacturer="삼성전자", memory_capacity=16))
+    elif (keyword == "배그용"):
+      cpus.extend(Cpu.objects.filter(cpu_manufacturer="인텔", cpu_core=6))
+      videocards.extend(Videocard.objects.filter(videocard_chipset="RTX4060Ti"))
+      memories.extend(Memory.objects.filter(memory_manufacturer="삼성전자", memory_capacity=32))
+    elif (keyword == "게임방송용"):
+      cpus.extend(Cpu.objects.filter(cpu_manufacturer="AMD", cpu_core=6))
+      videocards.extend(Videocard.objects.filter(videocard_chipset="RTX4070Ti"))
+      memories.extend(Memory.objects.filter(memory_manufacturer="삼성전자", memory_capacity=32))
+    elif (keyword == "코딩용" or keyword == "영상편집용"):
+      cpus.extend(Cpu.objects.filter(cpu_manufacturer="인텔", cpu_core=12))
+      if (keyword == "코딩용"):
+        videocards.extend(Videocard.objects.filter(videocard_chipset="GTX1660 SUPER"))
+      elif (keyword == "영상편집용"):
+        videocards.extend(Videocard.objects.filter(videocard_chipset="RTX4060Ti"))
+      memories.extend(Memory.objects.filter(memory_manufacturer="삼성전자", memory_capacity=32))   
+
+  # CPU, 그래픽카드, 메모리 성능 비교하기
+  try: # 입력한 키워드의 범위를 벗어난 경우 예외처리한다.
+    cpu_max = cpus[0].cpu_benchmark
+  except (ValueError, IndexError):
     return "exception"
+  try:
+    videocard_max = videocards[0].videocard_benchmark
+  except (ValueError, IndexError):
+    videocard_max = None
+  memory_max = memories[0].memory_capacity
+
+  for cpu in cpus:
+    if (cpu.cpu_benchmark > cpu_max):
+      cpu_max = cpu.cpu_benchmark
+  for videocard in videocards:
+    if (videocard.videocard_benchmark > videocard_max):
+      videocard_max = videocard.videocard_benchmark
+  for memory in memories:
+    if (memory.memory_capacity > memory_max):
+      memory_max = memory.memory_capacity
   
+  # CPU, 메인보드 세팅
+  if (cpu_max == 13520): 
+    cpus_set = Cpu.objects.filter(cpu_manufacturer="인텔", cpu_core=4)
+    mainboards_set = Mainboard.objects.filter(mainboard_chipset="H610")
+  elif (cpu_max == 19474): 
+    cpus_set = Cpu.objects.filter(cpu_manufacturer="인텔", cpu_core=6)
+    mainboards_set = Mainboard.objects.filter(mainboard_chipset="H610")
+  elif (cpu_max == 34742): 
+    cpus_set = Cpu.objects.filter(cpu_manufacturer="인텔", cpu_core=12)
+    mainboards_set = Mainboard.objects.filter(mainboard_chipset="B760")
+  elif (cpu_max == 28767): 
+    cpus_set = Cpu.objects.filter(cpu_manufacturer="AMD", cpu_core=6)
+    mainboards_set = Mainboard.objects.filter(mainboard_manufacturer="MSI", mainboard_chipset="A620")
+  elif (cpu_max == 39255): 
+    cpus_set = Cpu.objects.filter(cpu_manufacturer="AMD", cpu_core=12)
+    mainboards_set = Mainboard.objects.filter(mainboard_chipset="X570")
+
+  # 그래픽카드 세팅
+  if (videocard_max == 7860): videocards_set = Videocard.objects.filter(videocard_chipset="GTX1650")
+  elif (videocard_max == 12784): videocards_set = Videocard.objects.filter(videocard_chipset="GTX1660 SUPER")
+  elif (videocard_max == 17117): videocards_set = Videocard.objects.filter(videocard_chipset="RTX3060")
+  elif (videocard_max == 22544): videocards_set = Videocard.objects.filter(videocard_chipset="RTX4060Ti")
+  elif (videocard_max == 31649): videocards_set = Videocard.objects.filter(videocard_chipset="RTX4070Ti")
+  elif (videocard_max == 34727): videocards_set = Videocard.objects.filter(videocard_chipset="RTX4080")
+
+  # 메모리 세팅
+  if (memory_max == 16): memoires_set = Memory.objects.filter(memory_manufacturer="삼성전자", memory_capacity=16)
+  elif (memory_max == 32): memoires_set = Memory.objects.filter(memory_manufacturer="삼성전자", memory_capacity=32)
+
   # 저장공간 설정
-  if (keyword == "웹서핑용" or keyword == "영상시청용" or keyword == "사무용"):
-    storages = Storage.objects.filter(storage_device="HDD", storage_capacity="2TB")
-  else:
-    storages = Storage.objects.filter(storage_device="SSD", storage_capacity="500GB")
+  if (cpu_max == 13520):
+    storages_set = Storage.objects.filter(storage_device="HDD", storage_capacity="2TB")
+  elif (cpu_max == 19474 or cpu_max == 28767):
+    storages_set = Storage.objects.filter(storage_device="SSD", storage_capacity="500GB")
+  elif (cpu_max == 34742 or cpu_max == 39255 or videocard_max == 31649 or videocard_max == 34727):
+    storages_set = Storage.objects.filter(storage_device="SSD", storage_capacity="1TB")
 
   # 케이스 설정
-  if (keyword == "웹서핑용" or keyword == "영상시청용" or keyword == "사무용" or keyword == "롤용" or keyword == "피파4용"):
-    comcases = Comcase.objects.filter(comcase_manufacturer="3RSYS", comcase_size="미니타워")
-  else:
-    comcases = Comcase.objects.filter(comcase_manufacturer="마이크로닉스", comcase_size="미들타워")
+  if (cpu_max == 13520):
+    comcases_set = Comcase.objects.filter(comcase_manufacturer="3RSYS", comcase_size="미니타워")
+  elif (cpu_max == 19474 or cpu_max == 28767 or cpu_max == 34742 or cpu_max == 39255):
+    comcases_set = Comcase.objects.filter(comcase_manufacturer="마이크로닉스", comcase_size="미들타워")
   
   # 쿨러 설정
-  if (keyword == "롤용" or keyword == "피파4용" or keyword == "코딩용" or keyword == "영상편집용"):
-    coolers = Cooler.objects.filter(cooler_manufacturer="쿨러마스터", cooler_cooling="공랭")
-  elif (keyword == "배그용" or keyword == "게임방송용"):
-    coolers = Cooler.objects.filter(cooler_manufacturer="3RSYS", cooler_cooling="수랭")
+  if (videocard_max == 12784 or videocard_max == 17117 or videocard_max == 22544):
+    coolers_set = Cooler.objects.filter(cooler_manufacturer="쿨러마스터", cooler_cooling="공랭")
+  elif (videocard_max == 31649 or videocard_max == 34727):
+    coolers_set = Cooler.objects.filter(cooler_manufacturer="3RSYS", cooler_cooling="수랭")
+  else:
+    coolers_set = None
 
   # PC 부품 전력량 및 가격 계산
   total_wattage = 0
   total_price = 0
   
-  for cpu in cpus:
-    total_wattage += cpu.cpu_wattage
-    total_price += cpu.cpu_price
-  if (keyword != "웹서핑용" and keyword != "영상시청용" and keyword != "사무용"):
-    for cooler in coolers:
-      total_wattage += cooler.cooler_wattage
-      total_price += cooler.cooler_price
-  for mainboard in mainboards:
-    total_wattage += mainboard.mainboard_wattage
-    total_price += mainboard.mainboard_price
-  for memory in memories:
-    total_wattage += memory.memory_wattage
-    total_price += memory.memory_price
-  if (keyword != "웹서핑용" and keyword != "사무용"):
-    for videocard in videocards:
-      total_wattage += videocard.videocard_wattage
-      total_price += videocard.videocard_price
-  for storage in storages:
-    total_wattage += storage.storage_wattage
-    total_price += storage.storage_price
-  for comcase in comcases:
-    total_price += comcase.comcase_price
+  for cpu_set in cpus_set:
+    total_wattage += cpu_set.cpu_wattage
+    total_price += cpu_set.cpu_price
+  if (coolers_set != None):
+    for cooler_set in coolers_set:
+      total_wattage += cooler_set.cooler_wattage
+      total_price += cooler_set.cooler_price
+  for mainboard_set in mainboards_set:
+    total_wattage += mainboard_set.mainboard_wattage
+    total_price += mainboard_set.mainboard_price
+  for memory_set in memoires_set:
+    total_wattage += memory_set.memory_wattage
+    total_price += memory_set.memory_price
+  if (videocard_max != None):
+    for videocard_set in videocards_set:
+      total_wattage += videocard_set.videocard_wattage
+      total_price += videocard_set.videocard_price
+  for storage_set in storages_set:
+    total_wattage += storage_set.storage_wattage
+    total_price += storage_set.storage_price
+  for comcase_set in comcases_set:
+    total_price += comcase_set.comcase_price
 
   # 파워 설정 및 가격 계산
   if (total_wattage < 300):
-    powers = Power.objects.filter(power_formfactors="ATX", power_output=600)
+    powers_set = Power.objects.filter(power_formfactors="ATX", power_output=600)
   elif (total_wattage >= 300 and total_wattage < 400):
-    powers = Power.objects.filter(power_formfactors="ATX", power_output=700)
+    powers_set = Power.objects.filter(power_formfactors="ATX", power_output=700)
   elif (total_wattage >= 400):
-    powers = Power.objects.filter(power_formfactors="ATX", power_output=850)
+    powers_set = Power.objects.filter(power_formfactors="ATX", power_output=850)
   
-  for power in powers:
-    total_price += power.power_price
+  for power_set in powers_set:
+    total_price += power_set.power_price
   
   quote_list = []
   quote_price = []
   quote_image = []
   
   # 설정한 사양을 리스트에 집어넣기
-  for cpu in cpus:
-    quote_list.append(cpu.cpu_title)
-    quote_price.append(cpu.cpu_price)
-    quote_image.append(cpu.cpu_image)
-  if (keyword != "웹서핑용" and keyword != "영상시청용" and keyword != "사무용"):
-    for cooler in coolers:
-      quote_list.append(cooler.cooler_title)
-      quote_price.append(cooler.cooler_price)
-      quote_image.append(cooler.cooler_image)
-  for mainboard in mainboards:
-    quote_list.append(mainboard.mainboard_title)
-    quote_price.append(mainboard.mainboard_price)
-    quote_image.append(mainboard.mainboard_image)
-  for memory in memories:
-    quote_list.append(memory.memory_title)
-    quote_price.append(memory.memory_price)
-    quote_image.append(memory.memory_image)
-  if (keyword != "웹서핑용" and keyword != "사무용"):
-    for videocard in videocards:
-      quote_list.append(videocard.videocard_title)
-      quote_price.append(videocard.videocard_price)
-      quote_image.append(videocard.videocard_image)
-  for storage in storages:
-    quote_list.append(storage.storage_title)
-    quote_price.append(storage.storage_price)
-    quote_image.append(storage.storage_image)
-  for power in powers:
-    quote_list.append(power.power_title)
-    quote_price.append(power.power_price)
-    quote_image.append(power.power_image)
-  for comcase in comcases:
-    quote_list.append(comcase.comcase_title)
-    quote_price.append(comcase.comcase_price)
-    quote_image.append(comcase.comcase_image)
+  for cpu_set in cpus_set:
+    quote_list.append(cpu_set.cpu_title)
+    quote_price.append(cpu_set.cpu_price)
+    quote_image.append(cpu_set.cpu_image)
+  if (coolers_set != None):
+    for cooler_set in coolers_set:
+      quote_list.append(cooler_set.cooler_title)
+      quote_price.append(cooler_set.cooler_price)
+      quote_image.append(cooler_set.cooler_image)
+  for mainboard_set in mainboards_set:
+    quote_list.append(mainboard_set.mainboard_title)
+    quote_price.append(mainboard_set.mainboard_price)
+    quote_image.append(mainboard_set.mainboard_image)
+  for memory_set in memoires_set:
+    quote_list.append(memory_set.memory_title)
+    quote_price.append(memory_set.memory_price)
+    quote_image.append(memory_set.memory_image)
+  if (videocard_max != None):
+    for videocard_set in videocards_set:
+      quote_list.append(videocard_set.videocard_title)
+      quote_price.append(videocard_set.videocard_price)
+      quote_image.append(videocard_set.videocard_image)
+  for storage_set in storages_set:
+    quote_list.append(storage_set.storage_title)
+    quote_price.append(storage_set.storage_price)
+    quote_image.append(storage_set.storage_image)
+  for power_set in powers_set:
+    quote_list.append(power_set.power_title)
+    quote_price.append(power_set.power_price)
+    quote_image.append(power_set.power_image)
+  for comcase_set in comcases_set:
+    quote_list.append(comcase_set.comcase_title)
+    quote_price.append(comcase_set.comcase_price)
+    quote_image.append(comcase_set.comcase_image)
 
   quote_total_wattage = [total_wattage]
   quote_total_price = [total_price]
   
   # 설정한 사양을 반환하기
-  return "test"
+  return quote_list, quote_price, quote_image, quote_total_wattage, quote_total_price
     
 # 키워드 검색 함수
 def searchKeyword(keyword):
   # 예외 처리
-  usage = "용도"
-  usage2 = []
+  usage = []
 
   # 키워드 검색하기
   if ("롤" in keyword or "리그오브레전드" in keyword or "LOL" in keyword):
-    usage = "롤용"
-    usage2.append("롤용")
+    usage.append("롤용")
   if ("피파" in keyword or "피온" in keyword or "피파" in keyword):
-    usage = "피파4용"
-    usage2.append("피파4용")
+    usage.append("피파4용")
   if ("배그" in keyword or "배틀그라운드" in keyword or "PUBG" in keyword):
-    usage = "배그용"
-    usage2.append("배그용")
+    usage.append("배그용")
   if ("게임방송" in keyword or "스트리머" in keyword or "BJ" in keyword):
-    usage = "게임방송용"
-    usage2.append("게임방송용")
+    usage.append("게임방송용")
   if ("웹서핑" in keyword or "검색" in keyword or "인터넷" in keyword):
-    usage = "웹서핑용"
-    usage2.append("웹서핑용")
+    usage.append("웹서핑용")
   if ("인강" in keyword or "영상시청" in keyword or "강의" in keyword):
-    usage = "영상시청용"
-    usage2.append("영상시청용")
+    usage.append("영상시청용")
   if ("문서" in keyword or "사무" in keyword or "액셀" in keyword):
-    usage = "사무용"
-    usage2.append("사무용")
+    usage.append("사무용")
   if ("코딩" in keyword or "개발" in keyword or "프로그래" in keyword):
-    usage = "코딩용"
-    usage2.append("코딩용")
+    usage.append("코딩용")
   if ("영상편집" in keyword or "프리미어" in keyword or "영상작업" in keyword):
-    usage = "영상편집용"
-    usage2.append("영상편집용")
+    usage.append("영상편집용")
   if ("슬림" in keyword or "미니" in keyword):
-    usage = "슬림형"
-    usage2.append("슬림형")
+    usage.append("슬림형")
   if ("저소음" in keyword or "무소음" in keyword):
-    usage = "저소음"
-    usage2.append("저소음")
+    usage.append("저소음")
 
-  print(usage2)
   return usage
 
 # 메인 함수
